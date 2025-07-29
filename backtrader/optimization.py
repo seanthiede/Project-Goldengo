554a5660f01979a0d10ad5a2759db8f265e1ae3f
seanthiede <seanthiede@hotmail.com>
2025-07-29 11:44:09 +0200

    backtester documentation + optimization, backtester prepare data file

--- backtrader/optimization.py
+++ backtrader/optimization.py
@@ -10,64 +10,60 @@ import sys
 from prepare_data1 import load_and_prepare_data
 
 
-"""
-sma-indicator
-self.sma = SimpleMovingAverage(...)
-
-index approach: access current value
-av = self.sma[0]
-
-last value
-previous_value = self.sma[-1]
-"""
-
-class TestStrat(bt.Strategy):
+# Create a Stratey
+class TestStrategy(bt.Strategy):
     params = (
         ('maperiod', 15),
+        ('printlog', False),
     )
-    def log(self, txt, dt=None):
-        # Logging function for this strat
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+
+    def log(self, txt, dt=None, doprint=False):
+        ''' Logging function fot this strategy'''
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
-        # Keep reference to the "close" line in the data[0] dataseries
+        # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
-        # To keep track of pending orders
+
+        # To keep track of pending orders and buy price/commission
         self.order = None
         self.buyprice = None
         self.buycomm = None
 
-        self.sma = bt.indicators.MovingAverageSimple(self.datas[0], period=self.params.maperiod)
-
-        # Indicators for plotting to show
-        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
-        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=True)
-        bt.indicators.StochasticSlow(self.datas[0])
-        bt.indicators.MACDHisto(self.datas[0])
-        rsi = bt.indicators.RSI(self.datas[0])
-        bt.indicators.SmoothedMovingAverage(rsi, period=10)
-        bt.indicators.ATR(self.datas[0], plot=False)
+        # Add a MovingAverageSimple indicator
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.maperiod)
 
     def notify_order(self, order):
-        # Buy/Sell order submitted/accepted to/by broker - Nothing to do
         if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
             return
-        
+
         # Check if an order has been completed
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, Price: %.2f, cost: %.2f, Comm %.2f' % (order.executed.price, order.executed.value, order.executed.comm))
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
-            else:
-                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' % (order.executed.price, order.executed.value, order.executed.comm))
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
 
             self.bar_executed = len(self)
 
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('Order Canceled/Margin/Rejected')
+
         # Write down: no pending order
         self.order = None
 
@@ -75,14 +71,14 @@ class TestStrat(bt.Strategy):
         if not trade.isclosed:
             return
 
-        self.log('OPERATION PROFIT, GROSS %.2fk NET %.2f' % (trade.pnl, trade.pnlcomm))
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
 
-    # next() will be called on each bar of the system clock (self.datas[0])
     def next(self):
-        # Log the closing price of series from reference
+        # Simply log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
 
-        # Check if an order is pending
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
             return
 
@@ -90,29 +86,38 @@ class TestStrat(bt.Strategy):
         if not self.position:
 
             # Not yet ... we MIGHT BUY if ...
-            # current close less than previous close
             if self.dataclose[0] > self.sma[0]:
-                # buy (with all possible default parameters)
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
+
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.buy()
+
         else:
-            # Already in the market ... we might sell
+
             if self.dataclose[0] < self.sma[0]:
-                # sell (with all possible default parameters)
+                # SELL, SELL, SELL!!! (with all possible default parameters)
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
 
-        
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f' %
+                 (self.params.maperiod, self.broker.getvalue()), doprint=True)
+
+
 if __name__ == '__main__':
-    # 1. instantiate Cerebro engine (engine has created broker instance in the background, has cash to start with)
+    # Create a cerebro entity
     cerebro = bt.Cerebro()
 
-    # Add strategy
-    cerebro.addstrategy(TestStrat)
+    # Add a strategy
+    strats = cerebro.optstrategy(
+        TestStrategy,
+        maperiod=range(10, 31))
 
-    # Load and prepare Data
+     # Load and prepare Data
     base_dir = os.path.dirname(os.path.abspath(__file__))
     datafile = os.path.join(base_dir, "..", "crypto_data", "BTC", "BTC-USD_1d_20_25.csv")
     prepared_df = load_and_prepare_data(datafile)
@@ -139,17 +144,3 @@ if __name__ == '__main__':
         print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
     else:
         print('Error while loading data')
-        
-    """
-    # Create a Data Feed
-    data = bt.feeds.YahooFinanceCSVData(
-        dataname=datapath,
-        # Do not pass values vefore this date
-        fromdate=datetime.datetime(2000, 1, 1),
-        todate=datetime.datetime(2000, 12, 31),
-        reverse=False)
-    """
-    
-    
-
-    
